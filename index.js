24edab60cf5cc1c5261f8edb74a3d5e77f6b924e
austinaeden <austin.mbogo@student.moringaschool.com>
2023-06-20 11:35:40 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(destination){
+    if (destination == 43){
+    return 1 ;
+    }
+    if (destination == 50){
+    return 8;
+    }
+    if (destination == 34){
+        return 8;
+    }
+}
+
+function distanceFromHqInFeet(destination){
+    if (destination == 43){
+        return 264
+    }
+    if (destination == 50){
+        return 2112
+    }
+    if (destination == 34){
+        return 2112
+    }
+}
+
+function distanceTravelledInFeet(start, destination){
+    if (start == 43, destination == 48){
+        return 1320
+    }
+    if (start == 50, destination == 60){
+        return 2640
+    }
+    if (start == 34, destination == 28){
+        return 1584
+    }
+}
+
+function calculatesFarePrice(start, destination){
+    if (start == 43, destination == 44){
+        return 0
+    }
+    if (start == 34, destination == 32){
+        return 2.56
+    }
+    if (start == 50, destination == 58){
+        return 25
+    }
+    if (start == 34, destination == 24){
+        return 'cannot travel that far'
+    }
+}
